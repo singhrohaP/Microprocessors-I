afb89035143999be14072221a966aae09c53b5ff
Prachi Singhroha <93190284+singhrohaP@users.noreply.github.com>
2022-02-22 19:11:28 +0000

    Update Solution1.s

--- Solution1.s
+++ Solution1.s
@@ -1,21 +1,3 @@
-@ Start of file Arithmetic1.s
-@
-@ Copyright 2020 University of Dublin. All Rights Reserved.
-@
-@ Write an arm program to evaluate 4 * x * x + 3 * x
-@ assume x is in r1 and store result in r0
-@ 
-	
-
-.text
-	
-start:
-	
-        MOV     r1, #0x8 
-	
-	
-@ Upload the code that goes between the dashed lines.
-@ -------------- start cut ------------------------
 
 	mul r2, r1, r1
         mov r5, #0x4
@@ -23,10 +5,3 @@ start:
 	mov r6, #0x3
 	mul r4, r6, r1
 	add r0, r3, r4
-@ -------------- end cut --------------------------
-
-stop:   B	stop
-.data
-.end
-@
-@ End of file Arithmetic1.s
