57b85a1c537dab80f8ae8963052b52652b725f6b
Prachi Singhroha <93190284+singhrohaP@users.noreply.github.com>
2022-02-22 19:11:51 +0000

    Update Solution1.s

--- Solution1.s
+++ Solution1.s
@@ -1,7 +1,6 @@
-
-	mul r2, r1, r1
-        mov r5, #0x4
-	mul r3, r2, r5
-	mov r6, #0x3
-	mul r4, r6, r1
-	add r0, r3, r4
+mul r2, r1, r1
+mov r5, #0x4
+mul r3, r2, r5
+mov r6, #0x3
+mul r4, r6, r1
+add r0, r3, r4
